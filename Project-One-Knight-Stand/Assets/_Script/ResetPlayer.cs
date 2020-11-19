3bd6b4188b1398e482ac7a154b99c6ec44226916
Ken C <zhikang.chen@georgebrown.ca>
2020-11-19 17:40:17 -0500

    Added scenes 2

---
+++ Project-One-Knight-Stand/Assets/_Script/ResetPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetPlayer : MonoBehaviour
+{
+    public Transform Player;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //Do something
+        Player.transform.position = new Vector3(0, 0, 0);
+    }
+
+    private void Update()
+    {
+
+    }
+}
